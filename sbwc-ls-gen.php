2c021a3d5abb2b1fa9b02560a8a7eac4d9aabfc3
vanbess <dev@silverbackdev.co.za>
2023-06-01 11:36:49 +0200

    Version bump

--- sbwc-ls-gen.php
+++ sbwc-ls-gen.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name:       SBWC Line Sheet Generator
  * Description:       Allows the generation of WooCommerce product line sheets and saving said line sheets to PDF for distribution
- * Version:           1.0.0
+ * Version:           1.0.1
  * Requires at least: 5.0
  * Requires PHP:      7.4
  * Author:            WC Bessinger
